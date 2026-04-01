63d08e49ba8a7b0539cb71386d355728f8efbc13
DaanSeegers <597129@edu.rocmn.nl>
2026-04-01 09:49:31 +0200

    font
    en styling

--- public/js_public/winkels.js
+++ public/js_public/winkels.js
@@ -5,9 +5,10 @@ fetch("../api/get_winkels.php")
         data.forEach(winkel => {
             const li = document.createElement("li");
             li.textContent = winkel.winkelnaam + " - " + winkel.categorie;
-            
+
             winkelList.appendChild(li);
 
         });
     })
     .catch(err => console.error("Error fetching winkels:", err));
+
--- public/winkels.php
+++ public/winkels.php
@@ -1,40 +1,47 @@
-<?php
 
-?>
-
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 <head>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet">
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>winkels view</title>
-    <link rel="stylesheet" href="/public/css/winkels.css" type="text/css">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/public/css/main.css">
+    <link rel="stylesheet" href="/public/css/header.css">
+    <link rel="stylesheet" href="/public/css/footer.css">
+    <link rel="stylesheet" href="/public/css/winkels.css">
+    <!--    <link rel="icon" href="./public/assets/logo_icon.png" type="image/png">-->
 </head>
 <body>
+<?php include_once __DIR__ . '/../component/header.php'; ?>
+
 <!-- tijdelijke link -->
+<br>
+<br>
+<br>
+
 <a href="/admin/admin.php">admin</a>
 <h1>hoi</h1>
 <input type="text" id="searchInput" placeholder="Zoek een winkel">
 <div id="zoek-wrapper">
-        <button>alle winkels</button>
-        <button>mode&accesoires</button>
-        <button>eten&drinken</button>
-        <button>supermarkt</button>
-        <button>electronica</button>
-        <button>wonen&livestyle</button>
-        <button>restaurant</button>
+    <button>alle winkels</button>
+    <button>mode&accesoires</button>
+    <button>eten&drinken</button>
+    <button>supermarkt</button>
+    <button>electronica</button>
+    <button>wonen&livestyle</button>
+    <button>restaurant</button>
 </div>
 <hr>
 
 <div id="found"></div>
 
-    <ul id="winkelList">
-        <!-- hier komen de winkels -->
-    </ul>
-    
+<ul id="winkelList">
+    <!-- hier komen de winkels -->
+</ul>
+<?php include_once __DIR__ . '/../component/footer.php'; ?>
+
+
 </body>
 <script src="../public/js_public/winkels.js"></script>
 </html>
